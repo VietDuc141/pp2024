7673bb2315cc61361c30fe43f6d57d597f846597
VietDuc <ducnvm.22bi13095@usth.edu.vn>
2024-03-14 15:32:35 +0700

    lab 3

--- 3.student.mark.oop.math.py
+++ 3.student.mark.oop.math.py
@@ -87,37 +87,38 @@ class MarksManager:
             total_credits += credit
         return gpa / total_credits
 
-    def main(self):
-        self.input_students()
-        self.input_courses()
-        while True:
-            print("1. Input marks")
-            print("2. List students")
-            print("3. List courses")
-            print("4. Show student marks for a given course")
-            print("5. Calculate average GPA for a given student")
-            print("6. Exit")
-            choice = input("Enter choice: ")
-            if choice == '1':
-                self.input_marks()
-            elif choice == '2':
-                self.list_students()
-            elif choice == '3':
-                self.list_courses()
-            elif choice == '4':
-                self.show_marks()
-            elif choice == '5':
-                student_id = input("Enter student ID: ")
-                found = False
-                for student in self.students:
-                    if student.id == student_id:
-                        found = True
-                        print("Average GPA:", self.calculate_gpa(student))
-                        break
-                if not found:
-                    print("Student not found.")
-            elif choice == '6':
-                break
-            else:
-                print("Invalid choice")
-
+def main():
+    mask = MarksManager()
+    mask.input_students()
+    mask.input_courses()
+    while True:
+        print("1. Input marks")
+        print("2. List students")
+        print("3. List courses")
+        print("4. Show student marks for a given course")
+        print("5. Calculate average GPA for a given student")
+        print("6. Exit")
+        choice = input("Enter choice: ")
+        if choice == '1':
+            mask.input_marks()
+        elif choice == '2':
+            mask.list_students()
+        elif choice == '3':
+            mask.list_courses()
+        elif choice == '4':
+            mask.show_marks()
+        elif choice == '5':
+            student_id = input("Enter student ID: ")
+            found = False
+            for student in mask.students:
+                if student.id == student_id:
+                    found = True
+                    print("Average GPA:", mask.calculate_gpa(student))
+                    break
+            if not found:
+                print("Student not found.")
+        elif choice == '6':
+            break
+        else:
+            print("Invalid choice") 
+main()
